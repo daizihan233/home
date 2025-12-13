c44c41f36590b403dc77e78f70aac9c80e0c1fb0
KuoHu <y066ydg5@duck.com>
2025-12-14 01:33:45 +0800

    Update Bg

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -32,21 +32,9 @@ const bgUrl = ref(null);
 const imgTimeout = ref(null);
 const emit = defineEmits(["loadComplete"]);
 
-// 壁纸随机数
-// 请依据文件夹内的图片个数修改 Math.random() 后面的第一个数字
-const bgRandom = Math.floor(Math.random() * 10 + 1);
-
 // 更换壁纸链接
 const changeBg = (type) => {
-  if (type == 0) {
-    bgUrl.value = `/images/background${bgRandom}.jpg`;
-  } else if (type == 1) {
-    bgUrl.value = "https://api.dujin.org/bing/1920.php";
-  } else if (type == 2) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/views";
-  } else if (type == 3) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/acg";
-  }
+  bgUrl.value = "https://t.alcy.cc/moez";
 };
 
 // 图片加载完成
